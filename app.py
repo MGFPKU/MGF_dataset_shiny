2bdef4f0c3fbb46b25e7e372eb2ee236c9493828
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2026-07-12 14:18:14 +0800

    Fix download button asymmetry

--- app.py
+++ app.py
@@ -75,21 +75,21 @@ app_ui = ui.page_fluid(
                     ),
                     ui.input_action_button(
                         "download",
-                        "",
-                        class_="download-icon",
-                        icon=ui.tags.svg(
+                        ui.tags.svg(
                             {
                                 "xmlns": "http://www.w3.org/2000/svg",
                                 "viewBox": "0 0 24 24",
                                 "fill": "currentColor",
                                 "height": "20",
                                 "width": "20",
+                                "aria-hidden": "true",
                             },
                             Tag(
                                 "path",
                                 d="M5 20h14v-2H5v2zm7-18v12l5-5h-3V4h-4v5H7l5 5V2z",
                             ),
                         ),
+                        class_="download-icon",
                     ),
                     class_="col-sm-2",  # mimic layout_columns spacing
                     style="display: flex; flex-direction: column; align-items: start; justify-content: end;",
@@ -130,6 +130,7 @@ app_ui = ui.page_fluid(
                     width: 20px;
                     height: 20px;
                     fill: #333;
+                    display: block;
                 }}
                 
                 .detail-buttons {{
